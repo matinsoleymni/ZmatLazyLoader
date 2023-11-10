ee4122d02b407d1e1d49e74c4e089d75e56bd4e7
Matin Soleymani <matinsoleymni@gmail.com>
2023-11-11 00:02:29 +0330

    adding lib files :)

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ZmatLazyLoader
-This is a library for lazyloading with vanilla js
+یک کتابخونه با جی اس خام برای بحث لیزی لودینگ
+
+
+برای استفاده از این کتابخونه باید وارد فولدر dist بشید و از اونجا دوتا فایل داریم که یکیش مینیفای شده هست یکیش نشده
+
+
+ما به صورت کلی 4 تا فانکشن داریم توی این کتابخونه
+
+---
+
+| function name | work | args |
+|:-------------:|:----:|:-----------:|
+| `load` | load you file | url(string) , options(object) |
+| `groupLoad` | load group off file's | files(array) , options(object) |
+| `addAttr` | adding element Attributes | element , attrs(object) |
+| `isLoaded` | element loaded or not | id(int , string)
+
+
+داکیومنت در اینده به شدت نزدیک کامل نوشته شده و در این ریپو پوش میشه :)
+
+متین سلیمانی
+Zmat24
---
+++ dist/zmatLazyLoader.js
@@ -0,0 +1,69 @@
+/**
+ * ! ZmatLazyLoader
+ * ? https://github.com/matinsoleymni/ZmatLazyLoader
+ *  MIT License
+ * @author MatinSoleymani
+ */
+
+/**
+ * 
+ * @param {Number | String} id 
+ * @param {Boolean} group 
+ * @returns String
+ */
+export function isLoaded(id , group){
+    if(document.querySelectorAll(`#${id}`).length > 0){
+        if(!group){
+            console.warn("Element was Loaded")
+        }
+        return "Loaded";
+    }
+}
+
+/**
+ * 
+ * @param {Element} element 
+ * @param {Object} elementAttrs 
+ * @returns Void
+ */
+export function addAttr(element , elementAttrs){
+    if(elementAttrs){
+        Object.keys(elementAttrs).forEach(attr => {
+            element.setAttribute(attr , elementAttrs[attr])
+        })
+    }
+}
+
+/**
+ * 
+ * @param {URL | String | File} url 
+ * @param {Object} options 
+ * @param {Boolean} group 
+ * @returns Void
+ */
+export function load(url , options , group = 0){
+    if(isLoaded(options.id ?? "zmatLoader" , group) == "Loaded" && !group){
+        return;
+    }
+    let myElem = document.createElement(options.type ?? "h1")
+    myElem.src = url
+    myElem.id = options.id ?? "zmatLoader"
+    myElem.setAttribute("class" , options.class ?? "")
+    addAttr(myElem , options.attr)
+    document.querySelector(`#${options.parent ?? "body"}`).appendChild(myElem)
+    console.group("Loaded")
+    console.info(myElem);
+    console.groupEnd()
+}
+
+/**
+ * 
+ * @param {Array} group
+ * @param {Object} options = {}
+ * @return Void
+ */
+export function groupLoad(group , options={}){
+    group.forEach(item => {
+        load(item[0] , item[1] , 1)
+    })
+}
